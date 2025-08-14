efd5de830427b7968ba2d4bf058791a90d27a2ff
2025101202010036 <viniciusfg.araujo@gmail.com>
2025-08-14 08:45:56 -0300

    - Add exercicios nivel 5

--- Intermediario/src/Nivel_5/Exercicio17.java
+++ Intermediario/src/Nivel_5/Exercicio17.java
@@ -1,4 +1,25 @@
 package Nivel_5;
 
 public class Exercicio17 {
+    public static void main(String[] args) {
+
+        System.out.println("Somar os elementos de cada linha e exibir a soma.");
+
+        int[][] matriz = {
+                {1, 2, 1},
+                {1, 1, 2},
+                {2, 1, 1}
+        };
+
+        int soma = 0;
+
+        for (int i = 0; i < matriz.length; i++) {
+            soma = 0;
+            for (int j = 0; j < matriz[i].length; j++) {
+                soma += matriz[i][j];
+            }
+            System.out.println("Linha " + i + " = "+ soma);
+        }
+
+    }
 }
