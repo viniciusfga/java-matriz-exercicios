0262fcc2977795209ee86eac8d27b742d4522e19
viniciusfga <viniciusfg.araujo@gmail.com>
2025-09-27 20:53:19 -0300

    - Add Exercicio Planilha

--- Intermediario/src/Nivel_6/Exercicio24.java
+++ Intermediario/src/Nivel_6/Exercicio24.java
@@ -1,4 +1,35 @@
 package Nivel_6;
 
+import java.util.Scanner;
+
 public class Exercicio24 {
+    /*
+    Simular uma planilha de notas (matriz alunos × disciplinas), calcular médias e aprovações.
+     */
+    static String[][] aprovacao(String aluno, String matricula, double notas) {
+        String[][] planilha = new String[3][3];
+
+
+        return planilha;
+    }
+
+    static void imprimirPlanilha() {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Aluno / Disciplinas / Notas: ");
+        String aluno = sc.nextLine();
+
+        String disciplina = sc.nextLine();
+
+        double notas = sc.nextDouble();
+
+        aprovacao(aluno, disciplina, notas);
+        imprimirPlanilha();
+
+        sc.close();
+    }
 }
