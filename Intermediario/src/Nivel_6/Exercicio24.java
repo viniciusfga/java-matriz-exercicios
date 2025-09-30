4b4d70aed4f54fad537f0bf2e3ca442089e0d401
viniciusfga <viniciusfg.araujo@gmail.com>
2025-09-29 22:12:14 -0300

    - Exercicio Planilha

--- Intermediario/src/Nivel_6/Exercicio24.java
+++ Intermediario/src/Nivel_6/Exercicio24.java
@@ -7,7 +7,7 @@ public class Exercicio24 {
     /*
     Simular uma planilha de notas (matriz alunos × disciplinas), calcular médias e aprovações.
      */
-    static String[][] aprovacao(String aluno, String disciplinas, double notas) {
+    static String[][] aprovacao(String[] aluno, String disciplinas, double[] notas) {
         String[][] planilha = new String[2][4];
 
         String[] cabecalho = {"Aluno","Disciplinas","Nota","Situação"};
@@ -17,15 +17,22 @@ public class Exercicio24 {
         }
 
         // DADOS DO ALUNO
-        planilha[1][0] = aluno;
+        for (int i = 1; i <= planilha[1].length - 1; i++) {
+            planilha[i][0] = cabecalho[i - 1];
+        }
+
         planilha[1][1] = disciplinas;
-        planilha[1][2] = Double.toString(notas);
+
+        for (int i = 1; i <= planilha[1].length - 1; i++) {
+            planilha[i][2] = notas[i];
+        }
+
         planilha[1][3] = (notas >= 6) ? "Aprovado" : "Reprovado";
 
         return planilha;
     }
 
-    static void mostrarPlanilha2(String[][] p) {
+    static void mostrarPlanilha(String[][] p) {
         String[] cabecalho = {"Aluno","Disciplinas","Nota","Situação"};
 
         for (int i = 0; i <= p[0].length - 1; i++) {
@@ -41,36 +48,26 @@ public class Exercicio24 {
         }
     }
 
-    static void mostrarPlanilha(String[][] planilha) {
-        // JTable precisa do cabeçalho separado da primeira linha
-        String[] cabecalho = planilha[0];
-        String[][] dados = new String[planilha.length - 1][cabecalho.length];
-
-        // Copia só os dados (sem o cabeçalho)
-        for (int i = 1; i < planilha.length; i++) {
-            dados[i - 1] = planilha[i];
-        }
-
-        JTable tabela = new JTable(dados, cabecalho);
-        JScrollPane scrollPane = new JScrollPane(tabela);
-
-        JFrame frame = new JFrame("Planilha de Notas");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(scrollPane);
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Aluno / Disciplinas / Notas: ");
-        String aluno = sc.nextLine();
 
-        String disciplina = sc.nextLine();
 
-        double notas = sc.nextDouble();
+
+        System.out.println("Quantidade de Alunos: ");
+        int N  = sc.nextInt();
+
+        String[] aluno = new String[N];
+        double[] notas = new double[N];
+
+        System.out.println("Disciplina: ");
+        String disciplina = sc.next();
+
+        System.out.println("Nome e notas do aluno [i]:");
+        for (int i = 0; i < N; i++) {
+            aluno[i] = sc.next();
+            notas[i] = sc.nextDouble();
+        }
 
         String[][] planilha = aprovacao(aluno, disciplina, notas);
         mostrarPlanilha(planilha);
