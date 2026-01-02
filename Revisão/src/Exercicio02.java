db04207bb52bafa362a4640cb329923c1068a89b
Vinícius Araújo <viniciusfg.araujou@gmail.com>
2026-01-01 22:11:28 -0300

    Revisão

---
+++ Revisão/src/Exercicio02.java
@@ -0,0 +1,20 @@
+public class Exercicio02 {
+
+    public static void main() {
+
+        System.out.println("Imprimir uma matriz linha por linha usando dois for.");
+
+        int[][] matriz = {
+                {1,2},
+                {3,4}
+        };
+
+        for (int i = 0; i < matriz[0].length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
