9d7bebf25ba4a66a9014031fd1a74dd6d28786c0
Vinícius Araújo <viniciusfg.araujou@gmail.com>
2026-01-02 20:18:09 -0300

    Revisão

---
+++ Revisão/src/Exercicio06.java
@@ -0,0 +1,18 @@
+public class Exercicio06 {
+
+    static void main() {
+
+        System.out.println("Imprimir todos os índices (i, j) de uma matriz 3x3.");
+
+        int[][] matriz = new int[3][3];
+
+        for (int i = 0; i < matriz.length ; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.println("Índice (" + i + "," + j + ") ");
+            }
+        }
+
+
+
+    }
+}
