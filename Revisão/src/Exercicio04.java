2e57370599b1b6ceaa762f58b27f6f000f11fc10
Vinícius Araújo <viniciusfg.araujou@gmail.com>
2026-01-01 22:16:23 -0300

    Exercicio revisão

---
+++ Revisão/src/Exercicio04.java
@@ -0,0 +1,24 @@
+public class Exercicio04 {
+
+    static void main() {
+
+        System.out.println("Imprimir a matriz com os elementos separados por |.");
+
+        int[][] matriz = {
+                {1,2},
+                {3,4}
+        };
+
+        for (int i = 0; i < matriz[0].length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+                if (j < matriz[i].length - 1) {
+                    System.out.print(" | ");
+                }
+            }
+            System.out.println();
+        }
+
+
+    }
+}
