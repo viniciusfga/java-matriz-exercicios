f01142f3521da36cf2ac0e862070dae75bd1f224
Vinícius Araújo <viniciusfg.araujou@gmail.com>
2026-01-03 21:33:40 -0300

    Revisao

---
+++ Revisão/src/Exercicio8.java
@@ -0,0 +1,20 @@
+public class Exercicio8 {
+
+    static void main() {
+
+        System.out.println("Preencher uma matriz 3x1 com a tabuada do número 2.");
+
+        int[][] matriz = new int[3][1];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+
+                matriz[i][j] = 2 * (i + 1);
+
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
