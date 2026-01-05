ceea60c5ed0b530c11b1e677af6362da7012fbd8
Vinícius Araújo <viniciusfg.araujou@gmail.com>
2026-01-04 21:14:54 -0300

    Revisão

---
+++ Revisão/src/Exercicio09.java
@@ -0,0 +1,19 @@
+public class Exercicio09 {
+
+    static void main() {
+
+        System.out.println("Criar uma matriz 3x3 em que os valores são iguais à soma dos índices (i + j).");
+
+        int[][] matriz = new int[3][3];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = i + j;
+                System.out.printf("%2d", matriz[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+
+}
