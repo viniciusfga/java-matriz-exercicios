e6292b77d5ac575517fe0c11cebb5bb9b2943d49
Vinícius Araújo <viniciusfg.araujou@gmail.com>
2026-01-07 22:43:18 -0300

    Revisão

--- Revisão/src/Exercicio18N5.java
+++ Revisão/src/Exercicio18N5.java
@@ -1,6 +1,5 @@
-package Nivel_5;
+public class Exercicio18N5 {
 
-public class Exercicio19 {
     public static void main(String[] args) {
 
         System.out.println("Transpor uma matriz (inverter linhas por colunas).");
@@ -13,7 +12,7 @@ public class Exercicio19 {
 
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
-                System.out.printf("%4d ", matriz[j][i]);
+                System.out.printf("%4d", matriz[j][i]);
             }
             System.out.println();
         }
